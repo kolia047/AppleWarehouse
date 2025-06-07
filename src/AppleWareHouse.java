f7b9c2c89a1fec84be460b24cd1307aa681c9249
Nikolay Pervushin <kolia047@mail.ru>
2025-06-07 23:41:12 +0500

    Create class AppleWareHouse

---
+++ src/AppleWareHouse.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleWareHouse {
+    private List<Apple> apples = getAllApples();
+
+    private List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+}
